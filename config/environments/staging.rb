0e9e3d4f9c12412b90c8a2b0013fa1418720a200
Mohith Thimmaiah <thimmaiah@gmail.com>
2020-06-14 15:41:03 +0530

    Removed references to old urls

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,7 +75,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "dev.connuct.co.uk" }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'], :protocol => 'http' }
+  config.action_mailer.asset_host = "https://#{ENV['HOST']}"
+
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
