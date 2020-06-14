6fb42903f7adcd217a21a3ee47a3d07ebc989451
Mohith Thimmaiah <thimmaiah@gmail.com>
2020-06-14 14:54:31 +0530

    Fixd bug n serializer

--- app/serializers/user_doc_serializer.rb
+++ app/serializers/user_doc_serializer.rb
@@ -1,11 +1,9 @@
-include Rails.application.routes.url_helpers
-
 class UserDocSerializer < ActiveModel::Serializer
   attributes :id, :name, :doc_type, :user_id, :verified, :doc, 
   :notes, :created_at, :updated_at, :not_available, :secure_doc_url
 
   def secure_doc_url
-  	return url_for(object.doc) 
+  	return Rails.application.routes.url_helpers.url_for(object.doc) 
   end
 
 end
