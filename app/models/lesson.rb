2b301be25e952935d6dd74f2e88dc40a3f03894e
Mohith Thimmaiah <thimmaiah@gmail.com>
2020-06-16 15:37:14 +0530

    Added validations to Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,9 +3,11 @@ class Lesson < ApplicationRecord
     after_save ThinkingSphinx::RealTime.callback_for(:lesson)
     before_save :embed
 
+    validates_presence_of :link, :link_type, :title
+
     TYPES = ["Video", "Image"]
 
     def embed
-        self.link = self.link.delete("?").gsub("watchv=", "embed/")
+        self.link = self.link.delete("?").gsub("watchv=", "embed/") if self.link_type == "Video"
     end
 end
